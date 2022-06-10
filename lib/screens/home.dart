43673b7d239f5151bd200ecfe4727daea7ad6ff3
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2022-06-10 15:09:30 +0530

    removed appbar

---
+++ lib/screens/home.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mini_project/screens/customer_registration.dart';
+
+import '../home_page.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  List<Widget> containers = [
+      HomePage(),
+      HomePage(),
+  ];
+  final _textEditingController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar:AppBar(
+          title: Text("Digi Mess"),
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(
+                  text: "Customers",
+                ),
+                Tab(
+                  text: "Expences",
+                )
+              ],
+            )
+        ),
+        body: TabBarView(
+          children: containers,
+        ),
+        ),
+      );
+  }
+}
